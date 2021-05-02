4dda6f3935bb46e463322a0dc9c257f5f285bd97
Meghan Semrad <mlsemrad753@gmail.com>
2021-05-02 10:52:39 -0500

    made languageProficiency.cs model

--- LingaLearn/Models/Language.cs
+++ LingaLearn/Models/Language.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace LingaLearn.Models
+﻿namespace LingaLearn.Models
 {
     public class Language
     {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public string LanguageName { get; set; }
+        public int LanguageProficiencyId { get; set; }
+        public LanguageProficiency LanguageProficiency { get; set; }
     }
 }
---
+++ LingaLearn/Models/LanguageProficiency.cs
@@ -0,0 +1,8 @@
+﻿namespace LingaLearn.Models
+{
+    public class LanguageProficiency
+    {
+        public int Id { get; set; }
+        public string Proficiency { get; set; }
+    }
+}
