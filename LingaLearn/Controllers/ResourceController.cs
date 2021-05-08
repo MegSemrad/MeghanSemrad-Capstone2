cf07f0667223089a85aa01e99b7dbb82a7337c61
Meghan Semrad <mlsemrad753@gmail.com>
2021-05-08 18:04:29 -0500

    made resource controller with get method

---
+++ LingaLearn/Controllers/ResourceController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using LingaLearn.Models;
+using LingaLearn.Repositories;
+using Microsoft.AspNetCore.Authorization;
+
+namespace LingaLearn.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResourceController : ControllerBase
+    {
+
+        private readonly IResourceRepository _resourceRepository;
+        private readonly IUserRepository _userRepository;
+
+        public ResourceController(IResourceRepository resourceRepository, IUserRepository userRepository)
+        {
+            _resourceRepository = resourceRepository;
+            _userRepository = userRepository;
+        }
+
+
+
+
+
+        [HttpGet("GetResourcesByLanguageId/{LanguageId}")]
+        public IActionResult Get(int LanguageId)
+        {
+            return Ok(_resourceRepository.GetResourcesByLanguageId(LanguageId));
+        }
+
+
+
+
+
+        private User GetCurrentUser()
+        {
+            string FirebaseUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            return _userRepository.GetByFirebaseUserId(FirebaseUserId);
+        }
+    }
+}
