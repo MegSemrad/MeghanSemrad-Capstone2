56d5a923c9ca31e9094860c400b93d006211e53c
Meghan Semrad <mlsemrad753@gmail.com>
2021-05-08 17:25:37 -0500

    added models server side for resource types and resources

---
+++ LingaLearn/Models/Resource.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LingaLearn.Models
+{
+    public class Resource
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public int LanguageId { get; set; }
+
+        public int ResourceTypeId { get; set; }
+
+        public string Source { get; set; }
+    }
+}
---
+++ LingaLearn/Models/ResourceType.cs
@@ -0,0 +1,9 @@
+﻿namespace LingaLearn.Models
+{
+    public class ResourceType
+    {
+        public int Id { get; set; }
+
+        public string Type { get; set; }
+    }
+}
