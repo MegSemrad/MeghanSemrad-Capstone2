2f9a6dbfb2497ee22520c7e3457074db2f7a0781
Meghan Semrad <mlsemrad753@gmail.com>
2021-05-12 16:00:46 -0500

    worked on flashcard collection and flashcard add css

--- client/linga-learn/src/components/FlashcardCollections/FlashcardCollectionAndFlashcardAdd.js
+++ client/linga-learn/src/components/FlashcardCollections/FlashcardCollectionAndFlashcardAdd.js
@@ -86,68 +86,59 @@ const FlashcardCollectionAndFlashcardAdd = () => {
 
     return (
         <Container>
-            <Form>
-
-                <Container className="flashcardCollectionFormContainer">
-                    <Row form>
-                        <Col md={4}>
-                            <FormGroup row>
-                                <Label for="languageSelect" sm={2}>Language</Label>
-                                <Input type="select"
-                                    onChange={handleControlledInputChangeForFlashcardCollection}
-                                    id="languageId">
-                                    <option value="0" ></option>
-                                    {
-                                        languages.map(language => {
-                                            return (
-                                                <>
-                                                    <option key={language.id} value={language.id} >
-                                                        {language.languageName}
-                                                    </option>
-                                                </>
-                                            )
-                                        })
-                                    }
-                                </Input>
-                            </FormGroup>
-                        </Col>
 
 
-                        <Col md={4}>
-                            <FormGroup>
-                                <Label for="dateState">Date</Label>
-                                <Input
-                                    type="date"
-                                    onChange={handleControlledInputChangeForFlashcardCollection}
-                                    requiredAutoClassName="form-control"
-                                    id="date"
-                                    value={newFlashcardCollection.date} />
-                            </FormGroup>
-                        </Col>
+            <Row xs="3" className="flashcardFormsRow">
+                <Card className="flashcardCollectionFormCard">
+                    <Form >
+                        <FormGroup>
+                            <Label for="languageSelect" >Language</Label>
+                            <Input type="select"
+                                onChange={handleControlledInputChangeForFlashcardCollection}
+                                id="languageId">
+                                <option value="0" ></option>
+                                {
+                                    languages.map(language => {
+                                        return (
+                                            <>
+                                                <option key={language.id} value={language.id} >
+                                                    {language.languageName}
+                                                </option>
+                                            </>
+                                        )
+                                    })
+                                }
+                            </Input>
+                        </FormGroup>
 
+                        <FormGroup>
+                            <Label for="dateState">Date</Label>
+                            <Input
+                                type="date"
+                                onChange={handleControlledInputChangeForFlashcardCollection}
+                                requiredAutoClassName="form-control"
+                                id="date"
+                                value={newFlashcardCollection.date} />
+                        </FormGroup>
 
-                        <Col md={4}>
-                            <FormGroup>
-                                <Label for="topic">Topic</Label>
-                                <Input
-                                    type="topic"
-                                    onChange={handleControlledInputChangeForFlashcardCollection}
-                                    requiredAutoClassName="form-control"
-                                    id="topic"
-                                    value={newFlashcardCollection.topic} />
-                            </FormGroup>
-                        </Col>
+                        <FormGroup>
+                            <Label for="topic">Topic</Label>
+                            <Input
+                                type="topic"
+                                onChange={handleControlledInputChangeForFlashcardCollection}
+                                requiredAutoClassName="form-control"
+                                id="topic"
+                                value={newFlashcardCollection.topic} />
+                        </FormGroup>
 
-                    </Row>
+                        <Row>
+                            <Button id="hideFlashcardCollectionButtonVisibility" onClick={handleSaveFlashcardCollection}>Save</Button>
+                        </Row>
+                    </Form>
+                </Card>
 
-                    <Row>
-                        <Button id="hideFlashcardCollectionButtonVisibility" onClick={handleSaveFlashcardCollection}>Save</Button>
-                    </Row>
-                </Container>
-
-
-                <Container className="flashcardCollectionFormContainer">
-                    <Row id="AddFlashcardRowVisibility">
+                <Card className="flashcardFormCard" id="AddFlashcardRowVisibility">
+                    <Form>
                         <FormGroup>
                             <Label for="flashcard">Word</Label>
                             <Input
@@ -157,6 +148,7 @@ const FlashcardCollectionAndFlashcardAdd = () => {
                                 id="word"
                                 value={flashcard.word} />
                         </FormGroup>
+
                         <FormGroup>
                             <Label for="translation">Translation</Label>
                             <Input
@@ -166,31 +158,44 @@ const FlashcardCollectionAndFlashcardAdd = () => {
                                 id="translatedWord"
                                 value={flashcard.translatedWord} />
                         </FormGroup>
-                        <Button onClick={handleSaveFlashcard}>Add card</Button>
-                    </Row>
+
+                        <Row>
+                            <Button className="addFlashcardButton" onClick={handleSaveFlashcard}>Add card</Button>
+                        </Row>
+                    </Form>
+                </Card>
+            </Row>
 
 
-                    <Row className="flashcardContainer">
-                        {
-                            flashcards.map(newFlashcard => {
-                                return <>
+
+            <Row className="flashcardStudyTimeButton">
+                <Button id="AddStudyTimeButtonVisibility" onClick={() => {
+                    history.push(`/FlashcardList/${flashcardCollection.id}`)
+                }}>Study Time!</Button>
+            </Row>
+
+
+            <Container>
+                <Row className="flashcardContainer">
+                    {
+                        flashcards.map(newFlashcard => {
+                            return (
+                                <div className="flashcardDiv">
                                     <Card key={newFlashcard.id}>
                                         <CardBody>{newFlashcard.word}</CardBody>
                                     </Card>
                                     <Card>
                                         <CardBody>{newFlashcard.translatedWord}</CardBody>
                                     </Card>
-                                </>
-                            })
-                        }
-                    </Row>
-                </Container>
+                                </div>
+                            )
+                        })
+                    }
+                </Row>
+            </Container>
 
-                <Button id="AddStudyTimeButtonVisibility" onClick={() => {
-                    history.push(`/FlashcardList/${flashcardCollection.id}`)
-                }}>Study Time!</Button>
-            </Form >
-        </Container>
+
+        </Container >
     );
 }
 
